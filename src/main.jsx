3eb1a1454fc17254db3725078ca97c08011e064c
ivongr <itzelivongr@gmail.com>
2024-10-16 17:01:20 -0600

    update:agrega componente

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppHooks from './AppHooks.jsx'
+import MyButton from "./Button.jsx"
+import Profile  from "./Profile.jsx"
+import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppHooks/>
-  </StrictMode>,
+    <App/>
+    <MyButton/>
+    <MyButton/>
+    <MyButton/>
+    <Profile/>
+  </StrictMode>
 )
